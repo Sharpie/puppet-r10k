4523c720d8dfe79de37b8ee1d8d8f3d0c4b43fb8
Charlie Sharpsteen <source@sharpsteen.net>
2013-07-31 13:26:20 -0700

    Add unit tests for r10k::config
    Add tests for the `r10k::config` class that exercises file generation. These
    tests ensure:
    
      - Valid YAML is generated
    
      - The various hacks in the `r10k.yaml.erb` template are successfully
        concatenating multiple `to_yaml` dumps of Ruby objects.

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe 'r10k::config', :type => :class do
+  let(:params){
+    {
+      'sources' => {
+        'somename' => {
+          'remote'  => 'ssh://git@github.com/someuser/somerepo.git',
+          'basedir' => "${::settings::confdir}/environments"
+        },
+        'someothername' => {
+          'remote'  => 'ssh://git@github.com/someuser/someotherrepo.git',
+          'basedir' => '/some/other/basedir'
+        },
+      },
+      'purgedirs' => [
+        "${::settings::confdir}/environments",
+        '/some/other/basedir',
+      ],
+    }
+  }
+
+  it 'manages /etc/r10k.yaml' do
+    expect(subject).to contain_file('/etc/r10k.yaml')
+  end
+
+  it 'creates valid YAML' do
+    expect{ YAML.load subject.resource('File', '/etc/r10k.yaml')[:content]}.not_to raise_error
+  end
+
+  context 'when generating content' do
+    let(:content){ YAML.load subject.resource('File', '/etc/r10k.yaml')[:content] }
+
+    # These tests check that the template is properly concatenating multiple
+    # YAML dumps of Ruby objects.
+    it 'creates a hash of sources' do
+      expect(content[:sources]).to be_a(Hash)
+    end
+
+    it 'creates an array of purgedirs' do
+      expect(content[:purgedirs]).to be_a(Array)
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'mocha'
+require 'puppet'
+require 'rspec-puppet'
+
+PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
+FIXTURE_PATH = File.expand_path(File.join('spec', 'fixtures'), PROJECT_ROOT)
+
+$LOAD_PATH.unshift File.join(PROJECT_ROOT, 'lib')
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+
+  config.module_path = File.join(FIXTURE_PATH, 'modules')
+end
