0c7a2bc71f5b22d10c4d4a4aaa8862e9dfc54b17
Charlie Sharpsteen <source@sharpsteen.net>
2014-03-09 14:26:50 -0700

    Configure manifest directory in spec_helper
    This prevents the "No file(s) found for import of ... site.pp" error.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,5 @@ RSpec.configure do |config|
   config.mock_with :mocha
 
   config.module_path = File.join(FIXTURE_PATH, 'modules')
+  config.manifest_dir = File.join(FIXTURE_PATH, 'manifests')
 end
